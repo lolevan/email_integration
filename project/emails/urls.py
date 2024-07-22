b7cbc9f71492538521f57eec40d408568abbfdb8
Ivan <vanechka-nikitin-2004@mail.ru>
2024-07-22 14:43:46 +0800

    Create logic for 'fetch_emails' on ajax

--- project/emails/urls.py
+++ project/emails/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.email_list, name='email_list')
+    path('', views.email_list, name='email_list'),
+    path('fetch_emails/', views.fetch_emails, name='fetch_emails'),
 ]
--- project/emails/views.py
+++ project/emails/views.py
@@ -1,6 +1,21 @@
+import threading
+
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.management import call_command
 
 from .models import EmailMessage
+
+
+@csrf_exempt
+def fetch_emails(request):
+    def fetch():
+        call_command('fetch_emails')
+
+    thread = threading.Thread(target=fetch)
+    thread.start()
+    return JsonResponse({'status': 'Fetching emails started'})
 
 
 def email_list(request):
